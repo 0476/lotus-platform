73f530987ded9622d4c08420c1ea3c29ada43e63
徐大伟 <xudawei@lenztechretail.com>
2019-06-28 14:44:22 +0800

    增加监控中心

--- common-service/admin-web/src/main/java/com/ailikes/lotus/admin/AdminWebApplication.java
+++ common-service/admin-web/src/main/java/com/ailikes/lotus/admin/AdminWebApplication.java
@@ -2,10 +2,12 @@ package com.ailikes.lotus.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdminWebApplication.class, args);
---
+++ common-service/monitor-center/admin-server/src/main/java/com/ailikes/lotus/monitor/MonitorAdminServerApplication.java
@@ -0,0 +1,21 @@
+package com.ailikes.lotus.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/** 
+* @author 作者 owen E-mail: 624191343@qq.com
+* @version 创建时间：2017年12月8日 上午9:03:32 
+* 类说明 
+*/
+@EnableAdminServer
+@EnableDiscoveryClient 
+@SpringBootApplication  
+public class MonitorAdminServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorAdminServerApplication.class, args);
+	}
+
+}
