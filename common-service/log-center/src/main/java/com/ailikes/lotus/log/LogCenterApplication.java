94b4040bb0e85c4d9912ba5b2a0f3453990ddd1a
徐大伟 <xudawei@lenztechretail.com>
2019-06-28 15:11:25 +0800

    修复日志中心

--- common-service/log-center/src/main/java/com/ailikes/lotus/log/LogCenterApplication.java
+++ common-service/log-center/src/main/java/com/ailikes/lotus/log/LogCenterApplication.java
@@ -1,11 +1,15 @@
 package com.ailikes.lotus.log;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan({"com.ailikes.lotus.common","com.ailikes.lotus.log"})
+@MapperScan({"com.ailikes.lotus.**.dao"})
 public class LogCenterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LogCenterApplication.class, args);
